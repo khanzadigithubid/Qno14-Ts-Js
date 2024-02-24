17ec58bbce30e72979270e148b1b5dd0d1b53405
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:40:15 +0500

    Qno14 Ts/Js and node js of 45 Assignments
    Qno14 Ts/Js and node js of 45 Assignments

---
+++ Qno14.js
@@ -0,0 +1,7 @@
+"use strict";
+/*Guest List: If you could invite anyone, living or deceased, to dinner, who would you
+invite? Make a list that includes at least three people you’d like to invite to dinner.
+Then use your list to print a message to each person, inviting them to dinner.*/
+// Define the list of guests
+let guestsArr = ["Khanzadi", "Sapna", "Ayesha", "Kiran"];
+guestsArr.map((item) => (console.log(`Dear ${item}, You are Invited to a Dinner`)));
---
+++ Qno14.ts
@@ -0,0 +1,14 @@
+/*Guest List: If you could invite anyone, living or deceased, to dinner, who would you 
+invite? Make a list that includes at least three people you’d like to invite to dinner.
+Then use your list to print a message to each person, inviting them to dinner.*/
+
+// Define the list of guests
+let guestsArr: string[] = ["Khanzadi", "Sapna", "Ayesha", "Kiran"];
+
+guestsArr.map((item) => (
+    console.log(`Dear ${item}, You are Invited to a Dinner`)
+));
+
+
+
+  
